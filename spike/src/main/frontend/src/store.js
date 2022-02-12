c63c918cf2e54ba2845da84ecc92cf46fab4cc51
Marc Nuri <marc@marcnuri.com>
2022-02-12 08:01:29 +0100

    feat: Invalidate cache upon logout

--- spike/src/main/frontend/src/store.js
+++ spike/src/main/frontend/src/store.js
@@ -1,13 +1,22 @@
-import {configureStore} from '@reduxjs/toolkit';
-import {reducer as authReducer} from './auth';
+import {combineReducers, configureStore} from '@reduxjs/toolkit';
+import {logout, reducer as authReducer} from './auth';
 import {reducer as layoutReducer} from './layout';
 import {api as userApi} from './users';
 
+const appReducer = combineReducers({
+  auth: authReducer,
+  layout: layoutReducer,
+  [userApi.reducerPath]: userApi.reducer
+});
+
+const rootReducer = (state, action) => {
+  if (logout.match(action)) {
+    state = undefined;
+  }
+  return appReducer(state, action);
+};
+
 export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    layout: layoutReducer,
-    [userApi.reducerPath]: userApi.reducer
-  },
+  reducer: rootReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(userApi.middleware)
 });
