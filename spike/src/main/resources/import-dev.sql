d9e6d8b31c0b0d263d015854a345c0c62fb967b6
Marc Nuri <marc@marcnuri.com>
2022-02-12 08:01:53 +0100

    feat: provide different initial data per environment

--- spike/src/main/resources/import-dev.sql
+++ spike/src/main/resources/import-dev.sql
@@ -5,3 +5,8 @@ INSERT INTO "user_roles" ("id", "role") VALUES (0, 'admin')
     ON CONFLICT DO NOTHING;
 INSERT INTO "user_roles" ("id", "role") VALUES (0, 'user')
     ON CONFLICT DO NOTHING;
+INSERT INTO "users" ("id", "name", "password", "created", "version")
+    VALUES (1, 'user', '$2a$10$7b.9iLgXFVh.r1u9HEbMv.EDL3JcJgldsWHUg4etSUh4wCNGuExye', NOW(), 1)
+    ON CONFLICT DO NOTHING;
+INSERT INTO "user_roles" ("id", "role") VALUES (1, 'user')
+    ON CONFLICT DO NOTHING;
