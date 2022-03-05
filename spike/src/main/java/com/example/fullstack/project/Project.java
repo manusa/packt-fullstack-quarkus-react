f2293a664e190b26b95a72633dd6d01c40bd47e4
Marc Nuri <marc@marcnuri.com>
2022-03-05 11:59:35 +0100

    fix: Project name unique per user (not global)

--- spike/src/main/java/com/example/fullstack/project/Project.java
+++ spike/src/main/java/com/example/fullstack/project/Project.java
@@ -8,14 +8,20 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import javax.persistence.Version;
 import java.time.ZonedDateTime;
 
 @Entity
-@Table(name = "projects")
+@Table(
+  name = "projects",
+  uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"name", "user_id"})
+  }
+)
 public class Project extends PanacheEntity {
 
-  @Column(nullable = false, unique = true)
+  @Column(nullable = false)
   public String name;
 
   @ManyToOne(optional = false)
