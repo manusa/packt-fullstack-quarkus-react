d40b348653cfa55bcd8c768a18bc2dfedb6a8c34
Marc Nuri <marc@marcnuri.com>
2022-03-26 16:03:00 +0100

    feat: Unique key violation reported in exception handler

--- spike/src/main/java/com/example/fullstack/RestExceptionHandler.java
+++ spike/src/main/java/com/example/fullstack/RestExceptionHandler.java
@@ -1,19 +1,24 @@
 package com.example.fullstack;
 
+import io.vertx.pgclient.PgException;
 import org.hibernate.HibernateException;
 import org.hibernate.StaleObjectStateException;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.util.Objects;
+import java.util.Optional;
 
 @Provider
 public class RestExceptionHandler implements ExceptionMapper<HibernateException> {
 
+  private static final String PG_UNIQUE_VIOLATION_ERROR = "23505";
 
   @Override
   public Response toResponse(HibernateException exception) {
-    if (hasExceptionInChain(exception, StaleObjectStateException.class)) {
+    if (hasExceptionInChain(exception, StaleObjectStateException.class)
+      || hasPostgresErrorCode(exception, PG_UNIQUE_VIOLATION_ERROR)) {
       return Response.status(Response.Status.CONFLICT).build();
     }
     return Response
@@ -22,13 +27,23 @@ public class RestExceptionHandler implements ExceptionMapper<HibernateException>
       .build();
   }
 
-  private static boolean hasExceptionInChain(Throwable throwable, Class<?> exceptionClass) {
+  private static boolean hasExceptionInChain(Throwable throwable, Class<? extends Throwable> exceptionClass) {
+    return getExceptionInChain(throwable, exceptionClass).isPresent();
+  }
+
+  private static boolean hasPostgresErrorCode(Throwable throwable, String code) {
+    return getExceptionInChain(throwable, PgException.class)
+      .filter(ex -> Objects.equals(ex.getCode(), code))
+      .isPresent();
+  }
+
+  private static <T extends Throwable> Optional<T> getExceptionInChain(Throwable throwable, Class<T> exceptionClass) {
     while (throwable != null) {
       if (exceptionClass.isInstance(throwable)) {
-        return true;
+        return Optional.of((T) throwable);
       }
       throwable = throwable.getCause();
     }
-    return false;
+    return Optional.empty();
   }
 }
