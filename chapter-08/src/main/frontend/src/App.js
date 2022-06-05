1a1ce61a70bdf215f843600bb71ce0b610975b59
Marc Nuri <marc@marcnuri.com>
2022-06-05 12:04:35 +0200

    feat: Chapter 08 user management module

--- chapter-08/src/main/frontend/src/App.js
+++ chapter-08/src/main/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 import {Login} from './auth';
 import {InitialPage} from './InitialPage';
+import {Users} from './users';
 
 export const App = () => (
   <BrowserRouter>
@@ -9,6 +10,7 @@ export const App = () => (
       <Route exact path='/' element={<Navigate to='/initial-page' />} />
       <Route exact path='/login' element={<Login />} />
       <Route exact path='/initial-page' element={<InitialPage />} />
+      <Route exact path='/users' element={<Users />} />
     </Routes>
   </BrowserRouter>
 );
