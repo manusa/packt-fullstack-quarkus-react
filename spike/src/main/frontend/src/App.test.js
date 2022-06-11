15a07b4f220010a30ef5add3448b9b8cf02bb10f
Marc Nuri <marc@marcnuri.com>
2022-06-11 10:54:21 +0200

    test: added initial integration tests for the frontend

--- spike/src/main/frontend/src/App.test.js
+++ spike/src/main/frontend/src/App.test.js
@@ -1,8 +1,44 @@
-import { render, screen } from '@testing-library/react';
+import {screen} from '@testing-library/react';
+import {rest} from 'msw';
+import {setupServer} from 'msw/node';
+import {render} from './__tests__/react-redux';
+import {login} from './auth';
+import {store} from './store';
 import {App} from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Router tests', () => {
+  let server;
+  beforeAll(() => {
+    server = setupServer();
+    server.listen();
+  });
+  beforeEach(() => {
+    window.sessionStorage.clear();
+  });
+  afterAll(() => {
+    server.close();
+  })
+  test('logged out user, redirects to login', () => {
+    // Given
+    window.history.pushState({}, '', '/tasks');
+    // When
+    render(<App />);
+    // Then
+    expect(window.location.pathname).toEqual('/login');
+    expect(screen.getByText(/Sign in/, {selector: 'h1'}))
+      .toBeInTheDocument();
+  });
+  test('logged in user, displays tasks', async () => {
+    // Given
+    server.use(rest.post('/api/v1/auth/login', (req, res, ctx) =>
+      res(ctx.status(200), ctx.text('a-jwt'))));
+    await store.dispatch(login({name: 'user', password: 'password'}));
+    window.history.pushState({}, '', '/tasks');
+    // When
+    render(<App />);
+    // Then
+    expect(window.location.pathname).toEqual('/tasks');
+    expect(screen.getByText(/Task manager/, {selector: '.MuiTypography-h6'}))
+      .toBeInTheDocument();
+  });
+})
