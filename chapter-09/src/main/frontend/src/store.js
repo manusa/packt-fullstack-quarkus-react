54413fa87ae96e5b5627915a997c0b9d8f82463a
Marc Nuri <marc@marcnuri.com>
2022-07-04 15:27:01 +0200

    feat: Chapter 09 Adding project features

--- chapter-09/src/main/frontend/src/store.js
+++ chapter-09/src/main/frontend/src/store.js
@@ -1,11 +1,13 @@
 import {combineReducers, configureStore} from '@reduxjs/toolkit';
 import {logout, reducer as authReducer} from './auth';
 import {reducer as layoutReducer} from './layout';
+import {api as projectApi} from './projects';
 import {api as userApi} from './users';
 
 const appReducer = combineReducers({
   auth: authReducer,
   layout: layoutReducer,
+  [projectApi.reducerPath]: projectApi.reducer,
   [userApi.reducerPath]: userApi.reducer
 });
 
@@ -19,5 +21,6 @@ const rootReducer = (state, action) => {
 export const store = configureStore({
   reducer: rootReducer,
   middleware: getDefaultMiddleware => getDefaultMiddleware()
+    .concat(projectApi.middleware)
     .concat(userApi.middleware)
 });
